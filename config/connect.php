f8d77940360a5d894b71df2cacc9a64d58fb3414
Modestas Semionovas <modest.sem.code@gmail.com>
2024-11-26 23:26:49 +0200

    First commit

---
+++ config/connect.php
@@ -0,0 +1,17 @@
+<?php
+// Dane do połączenia z bazą danych
+$servername = "localhost";  // Nazwa serwera
+$username = "root";         // Nazwa użytkownika bazy danych
+$password = "root";             // Hasło użytkownika bazy danych
+$dbname = "online_test_system";      // Nazwa bazy danych, z którą chcesz się połączyć
+
+// Tworzymy połączenie
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Sprawdzamy połączenie
+if ($conn->connect_error) {
+    die("Połączenie nieudane: " . $conn->connect_error);
+} else {
+    echo "Połączono z bazą danych!";
+}
+?>
