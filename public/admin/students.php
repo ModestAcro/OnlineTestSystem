6f6aec6b51947a99d11225016286a132d67fdb75
Modestas Semionovas <modest.sem.code@gmail.com>
2024-12-14 00:01:43 +0200

    zmodyfikowalem header w student, teacher

--- public/admin/students.php
+++ public/admin/students.php
@@ -1,11 +1,29 @@
+<?php
+    session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Studenci</title>
+    <link rel="stylesheet" href="../../assets/css/main.css">
+    <title>Wykładowcy</title>
 </head>
 <body>
-    
+    <header>
+        <div class="left-header">
+            <a class="nav-btn" href="admin_dashboard.php">Strona główna</a>
+            <a class="nav-btn" href="teachers.php">Wykładowcy</a>
+        </div>
+        <div class="right-header">
+            <span class="name"><?php echo $_SESSION['imie'] . ' ' . $_SESSION['nazwisko']; ?></span>
+
+            <!-- Formularz wylogowania -->
+            <form action="../../config/logout.php" method="POST">
+                <button type="submit" class="logout-btn">Wyloguj</button>
+            </form>
+        </div>
+    </header>
 </body>
 </html>
