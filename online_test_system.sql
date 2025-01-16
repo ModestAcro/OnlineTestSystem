f4f866f1961a5ccb25d1004c2940d09088df4bfc
Modestas Semionovas <modest.sem.code@gmail.com>
2025-01-16 11:56:48 +0200

    Forma dodawania pytania

--- online_test_system.sql
+++ online_test_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 22, 2024 at 07:01 PM
+-- Generation Time: Jan 16, 2025 at 09:48 AM
 -- Server version: 8.0.35
 -- PHP Version: 8.2.20
 
@@ -40,8 +40,8 @@ CREATE TABLE `tAdministratorzy` (
 --
 
 INSERT INTO `tAdministratorzy` (`ID`, `imie`, `nazwisko`, `email`, `haslo`) VALUES
-(1, 'Jan', 'Kowalski', 'jan.kowalski@gmail.com', 'Jan'),
-(2, 'Karol', 'Kowalski', 'karol.kowalski@gmail.com', 'Karol');
+(8, 'Jan', 'Kowalski', 'jan.kowalski@gmail.com', '$2y$10$vDEZgC8JHU6SNFxux2nsWu4aSPbLnwAFhTt5Xog4s.2B1y5rkXzpe'),
+(9, 'Karol', 'Kowalski', 'karol.kowalski@gmail.com', '$2y$10$ki2Hj7Vnl2H9lZ85rT9XReNvhzMnao7hKiKvy6cn0zYUiCBmwIls2');
 
 -- --------------------------------------------------------
 
@@ -52,11 +52,20 @@ INSERT INTO `tAdministratorzy` (`ID`, `imie`, `nazwisko`, `email`, `haslo`) VALU
 CREATE TABLE `tGrupy` (
   `ID` int NOT NULL,
   `rok` year NOT NULL,
-  `uczelnia` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `przedmiot` varchar(255) NOT NULL,
   `nazwa` varchar(255) NOT NULL,
-  `id_wykladowcy` int NOT NULL
+  `id_wykladowcy` int NOT NULL,
+  `id_przedmiotu` int NOT NULL,
+  `id_uczelni` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tGrupy`
+--
+
+INSERT INTO `tGrupy` (`ID`, `rok`, `nazwa`, `id_wykladowcy`, `id_przedmiotu`, `id_uczelni`) VALUES
+(60, '2023', 'Grupa 4', 19, 26, 2),
+(61, '2024', 'Grupa 1', 22, 26, 2),
+(62, '2024', 'Grupa 2', 22, 26, 1);
 
 -- --------------------------------------------------------
 
@@ -68,6 +77,51 @@ CREATE TABLE `tGrupyStudenci` (
   `id_grupy` int NOT NULL,
   `id_studenta` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tGrupyStudenci`
+--
+
+INSERT INTO `tGrupyStudenci` (`id_grupy`, `id_studenta`) VALUES
+(60, 33),
+(61, 33),
+(62, 33),
+(60, 34),
+(61, 34),
+(62, 34),
+(60, 35),
+(61, 35),
+(62, 35),
+(61, 37),
+(62, 37);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tOdpowiedzi`
+--
+
+CREATE TABLE `tOdpowiedzi` (
+  `ID` int NOT NULL,
+  `id_pytania` int NOT NULL,
+  `tresc` text NOT NULL,
+  `data_stworzenia` datetime DEFAULT CURRENT_TIMESTAMP,
+  `data_aktualizacji` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `correct` tinyint(1) NOT NULL,
+  `punkty` decimal(5,2) NOT NULL DEFAULT '0.00'
+) ;
+
+--
+-- Dumping data for table `tOdpowiedzi`
+--
+
+INSERT INTO `tOdpowiedzi` (`ID`, `id_pytania`, `tresc`, `data_stworzenia`, `data_aktualizacji`, `correct`, `punkty`) VALUES
+(359, 100, '4', '2025-01-11 15:37:46', '2025-01-11 15:45:44', 1, 10.00),
+(360, 100, '2', '2025-01-11 15:37:46', '2025-01-11 15:45:44', 0, 0.00),
+(361, 101, '4', '2025-01-16 11:42:19', '2025-01-16 11:42:19', 1, 10.00),
+(362, 101, '2', '2025-01-16 11:42:19', '2025-01-16 11:42:19', 0, 0.00),
+(363, 101, '5', '2025-01-16 11:42:19', '2025-01-16 11:42:19', 0, 0.00),
+(364, 101, '6', '2025-01-16 11:42:19', '2025-01-16 11:42:19', 0, 0.00);
 
 -- --------------------------------------------------------
 
@@ -86,13 +140,33 @@ CREATE TABLE `tPrzedmioty` (
 --
 
 INSERT INTO `tPrzedmioty` (`ID`, `nazwa`, `uwagi`) VALUES
-(11, 'Matematyka', 'Przedmiot podstawowy, wymaga dużego nakładu pracy.'),
-(12, 'Fizyka', 'Zajęcia wymagające praktycznego podejścia, eksperymenty.'),
-(13, 'Chemia', 'Przedmiot teoretyczny z elementami laboratoryjnymi.'),
-(14, 'Biologia', 'Część materiału obejmuje wyjścia terenowe i obserwacje.'),
-(15, 'Informatyka', 'Zajęcia obejmują programowanie i tworzenie aplikacji.'),
-(16, 'Baza danych', 'Przedmiot prowadzi dr. German Budnik '),
-(17, 'Chmury obliczeniowe', 'Przedmiot prowadzi Roman Awlasewicz');
+(25, 'Bezpieczeństwo systemów informatycznych', 'Prowadzący: Jan Suchodolski'),
+(26, 'Pracownia dyplomowa I', 'Prowadzący: Mariusz Żynel'),
+(28, 'Usługi w chmurze obliczeniowej	', 'Prowadzący: Roman Avlasevič');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tPytania`
+--
+
+CREATE TABLE `tPytania` (
+  `ID` int NOT NULL,
+  `id_przedmiotu` int NOT NULL,
+  `id_wykladowcy` int NOT NULL,
+  `typ` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `tresc` text NOT NULL,
+  `data_stworzenia` datetime DEFAULT CURRENT_TIMESTAMP,
+  `data_aktualizacji` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tPytania`
+--
+
+INSERT INTO `tPytania` (`ID`, `id_przedmiotu`, `id_wykladowcy`, `typ`, `tresc`, `data_stworzenia`, `data_aktualizacji`) VALUES
+(100, 26, 19, 'Wielokrotnego wyboru', '2 + 2 = ?', '2025-01-11 15:37:46', '2025-01-11 15:37:46'),
+(101, 26, 22, 'Wielokrotnego wyboru', '2 + 2 = ?', '2025-01-16 11:42:19', '2025-01-16 11:42:19');
 
 -- --------------------------------------------------------
 
@@ -116,36 +190,28 @@ CREATE TABLE `tStudenci` (
 --
 
 INSERT INTO `tStudenci` (`ID`, `nr_albumu`, `imie`, `nazwisko`, `email`, `haslo`, `aktywny`, `uwagi`) VALUES
-(1, 10001, 'Marcin', 'Jankowski', 'marcin.jankowski@student.edu', 'haslo123', 'T', 'Student aktywnie uczestniczy w zajęciach'),
-(2, 10002, 'Katarzyna', 'Mazurek', 'katarzyna.mazurek@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(3, 10003, 'Tomasz', 'Nowak', 'tomasz.nowak@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(4, 10004, 'Monika', 'Kowalska', 'monika.kowalska@student.edu', 'haslo123', 'T', 'Zawsze punktualna'),
-(5, 10005, 'Piotr', 'Wójcik', 'piotr.wojcik@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(6, 10006, 'Agnieszka', 'Duda', 'agnieszka.duda@student.edu', 'haslo123', 'T', 'Student wykazuje dużą motywację'),
-(7, 10007, 'Marek', 'Zieliński', 'marek.zielinski@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(8, 10008, 'Klaudia', 'Szymańska', 'klaudia.szymanska@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(9, 10009, 'Paweł', 'Wiśniewski', 'pawel.wisniewski@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(10, 10010, 'Elżbieta', 'Mazur', 'elzbieta.mazur@student.edu', 'haslo123', 'T', 'Niezbyt aktywna na zajęciach'),
-(11, 10011, 'Aleksandra', 'Król', 'aleksandra.krol@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(12, 10012, 'Sebastian', 'Bąk', 'sebastian.bak@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(13, 10013, 'Michał', 'Sikora', 'michal.sikora@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(14, 10014, 'Janina', 'Kwiatkowska', 'janina.kwiatkowska@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(15, 10015, 'Wojciech', 'Piotrowski', 'wojciech.piotrowski@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(16, 10016, 'Aneta', 'Zawisza', 'aneta.zawisza@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(17, 10017, 'Kamil', 'Bąk', 'kamil.bak@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(18, 10018, 'Magdalena', 'Pawlak', 'magdalena.pawlak@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(19, 10019, 'Andrzej', 'Jankowski', 'andrzej.jankowski@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(20, 10020, 'Zofia', 'Sikora', 'zofia.sikora@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(21, 10021, 'Piotr', 'Dąbrowski', 'piotr.dabrowski@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(22, 10022, 'Karolina', 'Kozłowska', 'karolina.kozlowska@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(23, 10023, 'Grzegorz', 'Majchrzak', 'grzegorz.majchrzak@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(24, 10024, 'Alicja', 'Brodowska', 'alicja.brodowska@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(25, 10025, 'Natalia', 'Borkowska', 'natalia.borkowska@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(26, 10026, 'Paweł', 'Kozak', 'pawel.kozak@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(27, 10027, 'Monika', 'Majewska', 'monika.majewska@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(28, 10028, 'Tomasz', 'Kaczmarek', 'tomasz.kaczmarek@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(29, 10029, 'Dorota', 'Kowal', 'dorota.kowal@student.edu', 'haslo123', 'T', 'Brak uwag'),
-(30, 10030, 'Marek', 'Sikora', 'marek.sikora@student.edu', 'haslo123', 'T', 'Brak uwag');
+(33, 87188, 'Modest', 'Siemionow', 'modest.sem@gmail.com', '$2y$10$777sZGU5YDkZ578Cjo/q1.CEIM5LkpmvaUjIylLIarMWTeD8mur.u', 'N', 'Informatyka 3 rok'),
+(34, 83240, 'Edgar', 'Makowski', 'edgar.makowski@example.com', '$2y$10$NmWKMOWC5VOC15e.nFRosOdTlq0q8t253jawBdlcolOiZKXKey5t2', 'N', 'Informatyka 3 rok'),
+(35, 86476, 'Bogusław', 'Pacyno', 'boguslaw.pacyno@example.com', '$2y$10$rLI0wuXepebzKI0QsUb.ZOiItvSa9lOstxbA5ijsgHrCwk2McSkru', 'N', 'Informatyka 3 rok'),
+(37, 85746, 'Justyna', 'Širvinskaja', 'justyna.sirvinskaja@example.com', '$2y$10$i4OrZTX2obEy/RHJXieQ/uTQjCKWBgZXSY91zPGpDRn34LoRKmRym', 'N', 'Informatyka 3 rok');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tTesty`
+--
+
+CREATE TABLE `tTesty` (
+  `ID` int NOT NULL,
+  `nazwa` varchar(255) NOT NULL,
+  `data_utworzenia` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `data_rozpoczecia` datetime NOT NULL,
+  `data_zakonczenia` datetime NOT NULL,
+  `czas_trwania` int NOT NULL,
+  `ilosc_prob` int NOT NULL,
+  `id_grupy` int DEFAULT NULL,
+  `id_wykladowcy` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -155,11 +221,11 @@ INSERT INTO `tStudenci` (`ID`, `nr_albumu`, `imie`, `nazwisko`, `email`, `haslo`
 
 CREATE TABLE `tUczelnie` (
   `ID` int NOT NULL,
-  `nazwa_uczelni` varchar(255) NOT NULL,
+  `nazwa` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `miasto` varchar(255) NOT NULL,
   `kraj` varchar(255) NOT NULL,
   `kontynent` varchar(255) NOT NULL,
-  `adres_uczelni` varchar(255) DEFAULT NULL,
+  `adres` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `uwagi` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -167,12 +233,9 @@ CREATE TABLE `tUczelnie` (
 -- Dumping data for table `tUczelnie`
 --
 
-INSERT INTO `tUczelnie` (`ID`, `nazwa_uczelni`, `miasto`, `kraj`, `kontynent`, `adres_uczelni`, `uwagi`) VALUES
-(1, 'Uniwersytet w Białymstoku', 'Białystok', 'Polska', 'Europa', 'ul. Marii Skłodowskiej-Curie 14', 'Najważniejsza uczelnia w północno-wschodniej Polsce'),
-(2, 'Filiia Uniwersytetu w Białymstoku w Wilnie', 'Wilno', 'Litwa', 'Europa', 'Aguonų g. 22', 'Pierwsza filia polskiego uniwersytetu poza granicami kraju'),
-(3, 'Uniwersytet Jagielloński', 'Kraków', 'Polska', 'Europa', 'ul. Gołębia 24', 'Najstarszy uniwersytet w Polsce'),
-(4, 'Stanford University', 'Stanford', 'USA', 'Ameryka Północna', '450 Serra Mall, Stanford, CA', 'Znany z badań w dziedzinie IT i przedsiębiorczości'),
-(8, 'Uniwersytet wilieński', 'Wilno', 'Litwa', 'Europa', 'Gedimino pr.', 'Najstarszy uniwersytet');
+INSERT INTO `tUczelnie` (`ID`, `nazwa`, `miasto`, `kraj`, `kontynent`, `adres`, `uwagi`) VALUES
+(1, 'Uniwersytet w Białymstoku ', 'Białystok', 'Polska', 'Europa', ' Świerkowa 20B', 'Najważniejsza uczelnia w północno-wschodniej Polsce'),
+(2, 'Filiia Uniwersytetu w Białymstoku w Wilnie', 'Wilno', 'Litwa', 'Europa', 'Aguonų g. 22', 'Pierwsza filia polskiego uniwersytetu poza granicami kraju');
 
 -- --------------------------------------------------------
 
@@ -195,16 +258,31 @@ CREATE TABLE `tWykladowcy` (
 --
 
 INSERT INTO `tWykladowcy` (`ID`, `imie`, `nazwisko`, `email`, `haslo`, `aktywny`, `uwagi`) VALUES
-(1, 'Jan', 'Kowalski', 'jan.kowalski@example.com', 'haslo123', 'T', 'Wykładowca matematyki'),
-(2, 'Anna', 'Nowak', 'anna.nowak@example.com', 'haslo456', 'T', 'Wykładowca fizyki'),
-(3, 'Piotr', 'Wiśniewski', 'piotr.wisniewski@example.com', 'haslo789', 'T', 'Wykładowca informatyki'),
-(4, 'Maria', 'Wójcik', 'maria.wojcik@example.com', 'haslo101', 'T', 'Wykładowca języka polskiego'),
-(5, 'Tomasz', 'Kaczmarek', 'tomasz.kaczmarek@example.com', 'haslo202', 'T', 'Wykładowca historii'),
-(6, 'Katarzyna', 'Szymańska', 'katarzyna.szymanska@example.com', 'haslo303', 'T', 'Wykładowca biologii'),
-(7, 'Jakub', 'Zieliński', 'jakub.zielinski@example.com', 'haslo404', 'T', 'Wykładowca chemii'),
-(8, 'Elżbieta', 'Lewandowska', 'elzbieta.lewandowska@example.com', 'haslo505', 'T', 'Wykładowca ekonomii'),
-(9, 'Marek', 'Dąbrowski', 'marek.dabrowski@example.com', 'haslo606', 'T', 'Wykładowca geografii'),
-(10, 'Zofia', 'Krawczyk', 'zofia.krawczyk@example.com', 'haslo707', 'T', 'Wykładowca sztuki');
+(19, 'Mieczysław', 'Muraszkiewicz', 'mieczyslaw.muraszkiewicz@example.com', '$2y$10$ZoU/.xoGRJD.2ks6kWaxfOyQFECUUp3w8a.S0DWIyQL/1bZomctKK', 'N', 'prof. dr. hab.'),
+(22, 'Mariusz', 'Żynel', 'mariusz.zynel@example.com', '$2y$10$zh4Cuo/oFmNZr4UKRUOqV.COCVaWL6h2BHN8FkNbZWwAcva0FIjeu', 'N', 'dr.'),
+(23, 'Jan', 'Suchodolski', 'jan.suchodolski@example.com', '$2y$10$ObdDfKfbOCdFEzEu36XHluuqBOAYXORNsXbU1hZT9q2RZqNDBgtxu', 'N', 'dr.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tZalaczniki`
+--
+
+CREATE TABLE `tZalaczniki` (
+  `ID` int NOT NULL,
+  `id_pytania` int DEFAULT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `data_stworzenia` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `data_aktualizacji` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tZalaczniki`
+--
+
+INSERT INTO `tZalaczniki` (`ID`, `id_pytania`, `file_path`, `data_stworzenia`, `data_aktualizacji`) VALUES
+(44, 100, '../../uploads/19/security2.png', '2025-01-11 20:30:04', '2025-01-11 20:30:04'),
+(45, 101, '../../uploads/22/security.png', '2025-01-16 09:42:19', '2025-01-16 09:42:19');
 
 --
 -- Indexes for dumped tables
@@ -222,7 +300,9 @@ ALTER TABLE `tAdministratorzy`
 --
 ALTER TABLE `tGrupy`
   ADD PRIMARY KEY (`ID`),
-  ADD KEY `fk_wykladowca_grupy` (`id_wykladowcy`);
+  ADD KEY `fk_wykladowca_grupy` (`id_wykladowcy`),
+  ADD KEY `FK_id_przedmiotu` (`id_przedmiotu`),
+  ADD KEY `FK_id_uczelni` (`id_uczelni`);
 
 --
 -- Indexes for table `tGrupyStudenci`
@@ -232,10 +312,25 @@ ALTER TABLE `tGrupyStudenci`
   ADD KEY `fk_grupy_studenci_studenci` (`id_studenta`);
 
 --
+-- Indexes for table `tOdpowiedzi`
+--
+ALTER TABLE `tOdpowiedzi`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `id_pytania` (`id_pytania`);
+
+--
 -- Indexes for table `tPrzedmioty`
 --
 ALTER TABLE `tPrzedmioty`
   ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tPytania`
+--
+ALTER TABLE `tPytania`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `id_wykladowcy` (`id_wykladowcy`),
+  ADD KEY `fk_przedmiotu` (`id_przedmiotu`);
 
 --
 -- Indexes for table `tStudenci`
@@ -243,6 +338,14 @@ ALTER TABLE `tPrzedmioty`
 ALTER TABLE `tStudenci`
   ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `tTesty`
+--
+ALTER TABLE `tTesty`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `id_grupy` (`id_grupy`),
+  ADD KEY `fk_wykladowca` (`id_wykladowcy`);
 
 --
 -- Indexes for table `tUczelnie`
@@ -257,6 +360,13 @@ ALTER TABLE `tWykladowcy`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `tZalaczniki`
+--
+ALTER TABLE `tZalaczniki`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `id_pytania` (`id_pytania`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -264,37 +374,61 @@ ALTER TABLE `tWykladowcy`
 -- AUTO_INCREMENT for table `tAdministratorzy`
 --
 ALTER TABLE `tAdministratorzy`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tGrupy`
 --
 ALTER TABLE `tGrupy`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+
+--
+-- AUTO_INCREMENT for table `tOdpowiedzi`
+--
+ALTER TABLE `tOdpowiedzi`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tPrzedmioty`
 --
 ALTER TABLE `tPrzedmioty`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `tPytania`
+--
+ALTER TABLE `tPytania`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
 
 --
 -- AUTO_INCREMENT for table `tStudenci`
 --
 ALTER TABLE `tStudenci`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT for table `tTesty`
+--
+ALTER TABLE `tTesty`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tUczelnie`
 --
 ALTER TABLE `tUczelnie`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tWykladowcy`
 --
 ALTER TABLE `tWykladowcy`
-  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `tZalaczniki`
+--
+ALTER TABLE `tZalaczniki`
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- Constraints for dumped tables
@@ -304,6 +438,8 @@ ALTER TABLE `tWykladowcy`
 -- Constraints for table `tGrupy`
 --
 ALTER TABLE `tGrupy`
+  ADD CONSTRAINT `FK_id_przedmiotu` FOREIGN KEY (`id_przedmiotu`) REFERENCES `tPrzedmioty` (`ID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_id_uczelni` FOREIGN KEY (`id_uczelni`) REFERENCES `tUczelnie` (`ID`) ON DELETE CASCADE,
   ADD CONSTRAINT `fk_wykladowca_grupy` FOREIGN KEY (`id_wykladowcy`) REFERENCES `tWykladowcy` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
@@ -312,6 +448,32 @@ ALTER TABLE `tGrupy`
 ALTER TABLE `tGrupyStudenci`
   ADD CONSTRAINT `fk_grupy_studenci_grupy` FOREIGN KEY (`id_grupy`) REFERENCES `tGrupy` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_grupy_studenci_studenci` FOREIGN KEY (`id_studenta`) REFERENCES `tStudenci` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tOdpowiedzi`
+--
+ALTER TABLE `tOdpowiedzi`
+  ADD CONSTRAINT `todpowiedzi_ibfk_1` FOREIGN KEY (`id_pytania`) REFERENCES `tPytania` (`ID`);
+
+--
+-- Constraints for table `tPytania`
+--
+ALTER TABLE `tPytania`
+  ADD CONSTRAINT `fk_przedmiotu` FOREIGN KEY (`id_przedmiotu`) REFERENCES `tPrzedmioty` (`ID`),
+  ADD CONSTRAINT `tpytania_ibfk_1` FOREIGN KEY (`id_wykladowcy`) REFERENCES `tWykladowcy` (`ID`);
+
+--
+-- Constraints for table `tTesty`
+--
+ALTER TABLE `tTesty`
+  ADD CONSTRAINT `fk_wykladowca` FOREIGN KEY (`id_wykladowcy`) REFERENCES `tWykladowcy` (`ID`),
+  ADD CONSTRAINT `ttesty_ibfk_1` FOREIGN KEY (`id_grupy`) REFERENCES `tGrupy` (`ID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `tZalaczniki`
+--
+ALTER TABLE `tZalaczniki`
+  ADD CONSTRAINT `tzalaczniki_ibfk_1` FOREIGN KEY (`id_pytania`) REFERENCES `tPytania` (`ID`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
